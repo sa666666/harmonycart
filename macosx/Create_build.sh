855ad882930b3787ee093123cb571bd1b301f1bf
stephena <stephena@64e51d22-2ad5-4ca0-8c35-ffecdf55094f>
2010-07-28 15:31:37 +0000

    Updated the OSX build script to work with spaces in filenames, and also when an image already exists (it deletes it before creating a new one).

--- macosx/Create_build.sh
+++ macosx/Create_build.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Creates a HarmonyCartOSX disk image (dmg) from the command line.
+# Creates a HarmonyCart disk image (dmg) from the command line.
 # usage:
 #    Create_build.sh <version>
 #
@@ -14,26 +14,32 @@ fi
 VER="$1"
 DMG="HarmonyCart-${VER}-macosx.dmg"
 DISK="/Volumes/HarmonyCart"
+DEST=~/Desktop/${DMG}
 
 echo "Creating ${DMG} file ..."
-gunzip -c template.dmg.gz > ${DMG}
+gunzip -c template.dmg.gz > "${DMG}"
 
 echo "Mounting ${DMG} file ..."
-hdiutil attach ${DMG}
+hdiutil attach "${DMG}"
 
 echo "Adding Qt framework ..."
 macdeployqt ../HarmonyCart.app/ -no-plugins
 
 echo "Copying documentation ..."
-ditto ../Announce.txt ../Changes.txt ../Copyright.txt ../License.txt ../Readme.txt ${DISK}
+ditto ../Announce.txt ../Changes.txt ../Copyright.txt ../License.txt ../Readme.txt "${DISK}"
 
 echo "Copying application and 'arm' files ..."
-cp -r ../HarmonyCart.app ${DISK}
-cp -r ../arm $DISK
+cp -r ../HarmonyCart.app "${DISK}"
+cp -r ../arm "$DISK"
 
 echo "Ejecting ${DMG} ..."
-hdiutil eject ${DISK}
+hdiutil eject "${DISK}"
+
+if [ -f "${DEST}" ]; then
+	echo "Removing duplicate image found on desktop ..."
+  rm -f "${DEST}"
+fi
 
 echo "Compressing image, moving to Desktop ..."
-hdiutil convert ${DMG} -format UDZO -imagekey zlib-level=9 -o ~/Desktop/${DMG}
-rm -f ${DMG}
+hdiutil convert "${DMG}" -format UDZO -imagekey zlib-level=9 -o "${DEST}"
+rm -f "${DMG}"
